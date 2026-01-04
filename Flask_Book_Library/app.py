c3e34e21095eceb81cc86adf46ff386990d46039
jaszostak <01168963@pw.edu.pl>
2026-01-04 23:43:19 +0100

    add unsafe functions in app.py - cicd verification / task2

--- Flask_Book_Library/app.py
+++ Flask_Book_Library/app.py
@@ -1,5 +1,17 @@
 from project import app
+from flask import request
+import subprocess
 
+@app.get("/debug/unsafe-eval")
+def unsafe_eval():
+    expr = request.args.get("expr", "2+2")
+    return str(eval(expr))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.get("/debug/unsafe-cmd")
+def unsafe_cmd():
+    cmd = request.args.get("cmd", "echo CI_test")
+    out = subprocess.check_output(cmd, shell=True, text=True)
+    return out
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
